89ecdec0c3d57b527cb1aa82cbacf4b360622b3c
SirGraybon <graydonritchie@gmail.com>
2023-05-06 18:18:56 -0400

    adds seed files

---
+++ db/seeds/02_quizzes.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO quizzes (id, creator_id, title, category_id, description, active)
+VALUES
+  (1, 1, 'World Capitals', 1, 'Test your knowledge of world capitals!', 1),
+  (2, 1, 'American Presidents', 2, 'Test your knowledge of U.S. presidents!', 1),
+  (3, 1, 'Mathematics', 3, 'Test your math skills!', 1),
+  (4, 1, 'Science', 4, 'Test your knowledge of science!', 1),
+  (5, 1, 'Music', 5, 'Test your knowledge of music!', 1),
+  (6, 1, 'Literature', 6, 'Test your knowledge of literature!', 1),
+  (7, 1, 'Movies', 7, 'Test your knowledge of movies!', 1),
+  (8, 1, 'Sports', 8, 'Test your knowledge of sports!', 1),
+  (9, 1, 'Art', 9, 'Test your knowledge of art!', 1),
+  (10, 1, 'Food and Drink', 10, 'Test your knowledge of food and drink!', 1),
+  (11, 1, 'TV Shows', 11, 'Test your knowledge of TV shows!', 1),
+  (12, 1, 'Animals', 12, 'Test your knowledge of animals!', 1),
+  (13, 1, 'Fashion', 13, 'Test your knowledge of fashion!', 1),
+  (14, 1, 'Technology', 14, 'Test your knowledge of technology!', 1),
+  (15, 1, 'History', 2, 'Test your knowledge of history!', 1);
---
+++ db/seeds/03_questions.sql
@@ -0,0 +1,86 @@
+INSERT INTO questions (id, quiz_id, question, correct_answer_id)
+VALUES
+  -- Questions for Quiz 1: World Capitals
+  (1, 1, 'What is the capital of Spain?', 1),
+  (2, 1, 'What is the capital of South Africa?', 3),
+  (3, 1, 'What is the former capital of Russia?', 5),
+
+  -- Questions for Quiz 2: American Presidents
+  (4, 2, 'Who was the first president of the United States?', 7),
+  (5, 2, 'Who was the president during the Civil War?', 8),
+  (6, 2, 'Who was the president during World War II?', 9),
+
+  -- Questions for Quiz 3: Mathematics
+  (7, 3, 'What is the value of pi?', 11),
+  (8, 3, 'What is the square root of 64?', 12),
+  (9, 3, 'What is 2 + 3?', 13),
+
+  -- Questions for Quiz 4: Science
+  (10, 4, 'What is the largest human organ?', 14),
+  (11, 4, 'What is the only metal that is liquid at room temperature?', 15),
+  (12, 4, 'What is the chemical symbol for gold?', 16),
+
+  -- Questions for Quiz 5: Music
+  (13, 5, 'Who was the lead singer of Queen?', 17),
+  (14, 5, 'Who was the artist behind "Rolling in the Deep"?', 19),
+
+  -- Questions for Quiz 6: Literature
+  (15, 6, 'Who wrote "Hamlet"?', 21),
+  (16, 6, 'Who wrote "1984"?', 22),
+  (17, 6, 'Who wrote "Harry Potter and the Philosopher''s Stone"?', 23),
+
+  -- Questions for Quiz 7: Movies
+  (18, 7, 'Who directed "Jurassic Park"?', 25),
+  (19, 7, 'Who directed "Avatar"?', 26),
+  (20, 7, 'What is the highest-rated movie on IMDb?', 27),
+
+  -- Questions for Quiz 8: Sports
+  (21, 8, 'What is the most popular sport in the world?', 29),
+  (22, 8, 'Who has won the most Wimbledon singles titles in history?', 31),
+
+  -- Questions for Quiz 9: Art
+  (23, 9, 'Who painted the Mona Lisa?', 33),
+  (24, 9, 'Which painting is by Vincent van Gogh?', 34),
+  (25, 9, 'Which painting is known for its swirling sky?', 36),
+
+  -- Questions for Quiz 10: Food and Drink
+  (26, 10, 'Which cuisine is sushi from?', 37),
+  (27, 10, 'Which cuisine is tacos from?', 38),
+  (28, 10, 'Which wine is made from the Cabernet Sauvignon grape?', 39),
+
+
+  -- Questions for Quiz 11: TV Shows
+  (29, 11, 'Which TV show features a high school chemistry teacher who becomes a drug dealer?', 41),
+  (30, 11, 'Which TV show features a New Jersey mob boss?', 42),
+  (31, 11, 'Which TV show depicts a group of friends?', 43),
+  (32, 11, 'Which TV show depicts Seinfeld?', 44),
+
+
+  -- Questions for Quiz 12: Animals
+  (33, 12, 'Which of the following is the largest land mammal?', 45),
+  (34, 12, 'Which of the following does not eat meat?', 48),
+  (35, 12, 'Which of these birds is flightless?', 43),
+  (36, 12, 'Which of these has a pouch?', 44),
+
+
+  -- Questions for Quiz 13: Fashion
+  (37, 13, 'the devil wears...?', 57),
+  (38, 13, 'Which popular brand originated in Italy?', 59),
+  (39, 13, 'Which garment is often sean ripped or torn on purpose?', 62),
+
+
+  -- Questions for Quiz 14: Technology
+  (40, 14, 'What does "HTML" stand for?', 65),
+  (41, 14, 'the iPhone released in what year?', 70),
+  (42, 14, 'Which of the following is the correct SQL query order', 73),
+
+
+
+  -- Questions for Quiz 15: History
+  (43, 15, 'The statue of Libery was built in which country before being brought to the USA?', 74),
+  (44, 15, 'World War One officilly ended on which date?', 78),
+  (45, 15, 'What year did we first land on the moon?', 81),
+
+
+
+
