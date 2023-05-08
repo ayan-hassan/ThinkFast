2a5007de03f1aabd6cb98e5965e34d29185783ff
SirGraybon <graydonritchie@gmail.com>
2023-05-08 11:02:33 -0400

    updats seeds and header styling

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,18 +1,35 @@
 
-INSERT INTO quizzes (id, creator_id, title, category_id, description, active)
+INSERT INTO quizzes (id, creator_id, title, category_id, description, active, created_at)
 VALUES
-  (1, 1, 'World Capitals', 1, 'Test your knowledge of world capitals!', 1),
-  (2, 1, 'American Presidents', 2, 'Test your knowledge of U.S. presidents!', 1),
-  (3, 1, 'Mathematics', 3, 'Test your math skills!', 1),
-  (4, 1, 'Science', 4, 'Test your knowledge of science!', 1),
-  (5, 1, 'Music', 5, 'Test your knowledge of music!', 1),
-  (6, 1, 'Literature', 6, 'Test your knowledge of literature!', 1),
-  (7, 1, 'Movies', 7, 'Test your knowledge of movies!', 1),
-  (8, 1, 'Sports', 8, 'Test your knowledge of sports!', 1),
-  (9, 1, 'Art', 9, 'Test your knowledge of art!', 1),
-  (10, 1, 'Food and Drink', 10, 'Test your knowledge of food and drink!', 1),
-  (11, 1, 'TV Shows', 11, 'Test your knowledge of TV shows!', 1),
-  (12, 1, 'Animals', 12, 'Test your knowledge of animals!', 1),
-  (13, 1, 'Fashion', 13, 'Test your knowledge of fashion!', 1),
-  (14, 1, 'Technology', 14, 'Test your knowledge of technology!', 1),
-  (15, 1, 'History', 2, 'Test your knowledge of history!', 1);
+  (1, 1, 'World Capitals', 1, 'Test your knowledge of world capitals!', 1,'2023-05-08 13:24:00'),
+  (2, 1, 'American Presidents', 2, 'Test your knowledge of U.S. presidents!', 1,'2023-05-07 19:42:13'),
+  (3, 1, 'Mathematics', 3, 'Test your math skills!', 1,'2023-05-06 08:30:59'),
+  (4, 1, 'Science', 4, 'Test your knowledge of science!', 1,'2023-05-05 16:55:27'),
+  (5, 1, 'Music', 5, 'Test your knowledge of music!', 1,'2023-05-04 11:20:44'),
+  (6, 1, 'Literature', 6, 'Test your knowledge of literature!', 1,'2023-05-03 14:37:08'),
+  (7, 1, 'Movies', 7, 'Test your knowledge of movies!', 1,'2023-05-02 22:18:02'),
+  (8, 1, 'Sports', 8, 'Test your knowledge of sports!', 1,'2023-05-01 07:09:35'),
+  (9, 1, 'Art', 9, 'Test your knowledge of art!', 1,'2023-04-30 23:59:59'),
+  (10, 1, 'Food and Drink', 10, 'Test your knowledge of food and drink!', 1,'2023-04-29 04:30:15'),
+  (11, 1, 'TV Shows', 11, 'Test your knowledge of TV shows!', 1,'2023-04-28 10:12:57'),
+  (12, 1, 'Animals', 12, 'Test your knowledge of animals!', 1,'2023-04-27 18:06:22'),
+  (13, 1, 'Fashion', 13, 'Test your knowledge of fashion!', 1,'2023-04-26 01:45:30'),
+  (14, 1, 'Technology', 14, 'Test your knowledge of technology!', 1,'2023-04-25 09:21:11'),
+  (15, 1, 'History', 2, 'Test your knowledge of history!', 1,'2023-04-24 17:00:00');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
